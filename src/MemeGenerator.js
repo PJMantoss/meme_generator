501b4858d0347641b716877098839cfa94d8b99d
PJMantoss <joelptoss@gmail.com>
2019-09-24 19:37:03 +0100

    Create Header and MemeGen components

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,13 +3,27 @@ import React from 'react';
 class MemeGenerator extends React.Component{
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
+        }
+    }
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+          .then(response => response.json())
+          .then(response => {
+              const {memes} = response.data
+              this.setState({allMemeImgs: memes})
+        })
     }
 
     render() {
 
         return(
-            <h1>MemeGenerator</h1>
+            <h1>MEME GENERATOR SECTION</h1>
         )
     }
 }
